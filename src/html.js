a5449a04de0ec605f517c632a51d97192da724e4
ken <harygato3@gmail.com>
2020-11-28 19:48:05 -0600

    added colors on chrome tabs

--- src/html.js
+++ src/html.js
@@ -5,6 +5,7 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
+        <meta name="theme-color" content="#85d5a4" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
