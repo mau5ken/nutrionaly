6cc7c8428d596552b5b1945744d379b03f78f78f
ken <harygato3@gmail.com>
2020-11-28 22:03:10 -0600

    added header design

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Navbar, Nav, Container } from "react-bootstrap";
+export default (props) => (
+
+    <Navbar bg="transparent" variant="light">
+        <Container>
+            <img
+                src="https://res.cloudinary.com/craftwebs/image/upload/v1605419794/nu07r10y3/PNG%27S/nutrionaly_logo_trans.png"
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+                style={{position: 'relative', top: '15px'}}
+            />
+            <Nav className="mr-auto">
+                <Nav.Link href="/">Inicio</Nav.Link>
+                <Nav.Link href="/blog">Blog</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar>
+)
