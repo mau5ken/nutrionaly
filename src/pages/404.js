15a74bdccb7e3edc869cbe8f8da8314410e3b79e
ken <harygato3@gmail.com>
2021-01-24 03:51:38 -0600

    added 404 site

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Button } from "react-bootstrap"
 
 class NotFoundPage extends React.Component {
   render() {
@@ -12,8 +12,15 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div className="text-center pt-5 align-middle">
+          <h1>¡Oh no!</h1>
+          <p>No hemos encontrado el sitio al que intentaste navegar. <br></br> 
+          Será mejor si regresamos al sitio principal. ¿Sale y vale?
+          </p>
+          <Button href="/" variant="dark">Regresar a Inicio</Button>{' '}
+          <br></br>
+          <img style={{width: "450px"}} className="mx-auto img-fluid pt-5" alt="404 not found" src={`../404.svg`}/>
+        </div>
       </Layout>
     )
   }
